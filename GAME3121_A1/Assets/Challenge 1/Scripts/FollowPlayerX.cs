40c7c9ee422fb3cc2ec23365011fc7fc7c9ba4fc
Jogo1001 <101247726@georgebrown.ca>
2025-09-23 14:38:15 -0400

    camera angle fixed

--- GAME3121_A1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ GAME3121_A1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -7,14 +7,12 @@ public class FollowPlayerX : MonoBehaviour
     public GameObject plane;
     private Vector3 offset;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        offset = new Vector3(0f, 5.21f, -14.57f); 
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate() 
     {
         transform.position = plane.transform.position + offset;
     }
