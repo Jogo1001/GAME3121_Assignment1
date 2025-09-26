27d2b1e957ce21535a52660b0b62ceeb249a72ed
Jogo1001 <101247726@georgebrown.ca>
2025-09-26 12:33:21 -0400

    ball random spawn fixed

--- GAME3121_A1_Challenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ GAME3121_A1_Challenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -6,9 +6,9 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = 13;
+    private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
-    private float spawnPosY = 0;
+    private float spawnPosY = 30;
 
 
     private float startDelay = 1.0f;
@@ -23,11 +23,13 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        // Assignment fixed 
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
